3b5c73385da1e27bbca510bc834b40a37a4a61ce
Jonathan Cho <joncho26@gmail.com>
2015-01-16 16:32:53 -0500

    add user edit profile

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,3 +3,21 @@ get '/' do
   erb :index
 end
 
+get '/user/:id' do
+  @user = User.find_by(id: params[:id])
+  erb :'user/user_profile'
+end
+
+
+#update(edit) a user profile
+get '/user/:id/edit' do
+  @user = User.find(params[:id])
+  erb :'user/edit'
+end
+
+put '/user/:id' do |id|
+  user = User.find(id)
+  user.update(params[:user])
+  redirect("/user/#{user.id}")
+end
+
