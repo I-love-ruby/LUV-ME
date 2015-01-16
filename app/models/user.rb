4cfdf24c64c69a1e66e37b13ffcddd740021a91d
Ricky Thomas <rickythomas.rt@gmail.com>
2015-01-16 14:45:13 -0500

    Create relations in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
+  has_many :messages
+
+  has_many :sent_messages, foreign_key: "sender_id", class_name: "Message"
+  has_many :received_messages, foreign_key: "receiver_id", class_name: "Message"
+
+  has_many :senders, through: :received_messages, source: :sender
+  has_many :receivers, through: :sent_messages, source: :receiver
+
+  has_one :survey
+
 end
