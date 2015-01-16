25a5bcbe5f7e96f853820bfceebe8612c0faadbc
Ricky Thomas <rickythomas.rt@gmail.com>
2015-01-16 16:24:45 -0500

    Refactor auth controller

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,28 +1,18 @@
-get '/login' do
-  erb :'auth/login'
-end
-
 post '/login' do
   user = User.find_by(name: params[:user][:name])
 
   if user.try(:authenticate, params[:user][:password])
     session[:user_id] = user.id
-    redirect '/'
-  else
-    redirect '/login'
   end
+  redirect '/'
 end
-
 
 post '/signup' do
   user = User.create(params[:user])
-
   if user.save
     session[:user_id] = user.id
-    redirect "/"
-  else
-    redirect "/"
   end
+  redirect "/"
 end
 
 get '/signout' do
