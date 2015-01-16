fffbfcbd330eb5bd2163029ef3e5c3de838d58f8
Ricky Thomas <rickythomas.rt@gmail.com>
2015-01-16 14:36:53 -0500

    Create migration for users

---
+++ db/migrate/20150116142714_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.integer :age
+      t.string :gender
+      t.string :location
+      t.string :password
+      t.string :password_digest
+      t.string :email
+      t.string :pic_url
+      t.string :tagline
+      t.text :about
+      t.timestamps
+    end
+  end
+end
