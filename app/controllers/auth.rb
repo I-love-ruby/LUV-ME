a4801d59573e169c42bcba752c8f13369c975664
Mai <mxngyn@gmail.com>
2015-01-18 02:09:38 -0500

    Change user auth controllers

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,16 +1,17 @@
 post '/login' do
-  user = User.find_by(name: params[:user][:email])
+  @user = User.find_by(name: params[:user][:email])
 
-  if user.try(:authenticate, params[:user][:password])
-    session[:user_id] = user.id
+  if @user.try(:authenticate, params[:user][:password])
+    session[:user_id] = @user.id
   end
-  redirect "/"
+  erb :home
 end
 
 post '/signup' do
   user = User.create(params[:user])
   if user.save
     session[:user_id] = user.id
+    redirect "/user/#{user.id}/edit"
   end
   redirect "/"
 end
