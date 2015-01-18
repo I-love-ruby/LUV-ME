36ee39a8d5c32acf56e1de8cf9c2466edade1005
Mai <mxngyn@gmail.com>
2015-01-18 02:09:58 -0500

    Remove duplicate user routes

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,24 +1,3 @@
-post '/login' do
-  user = User.find_by(name: params[:user][:email])
-
-  if user.try(:authenticate, params[:user][:password])
-    session[:user_id] = user.id
-  end
-  redirect "/"
-end
-
-post '/signup' do
-  user = User.create(params[:user])
-  if user.save
-    session[:user_id] = user.id
-  end
-  redirect "/"
-end
-
-get '/signout' do
-  session[:user_id] = nil
-  redirect '/'
-end
 
 #update(edit) a user profile
 get '/user/:id/edit' do
