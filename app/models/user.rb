8fa336367293a2300300d1cbc89a412a7488792b
Mai <mxngyn@gmail.com>
2015-01-18 05:54:04 -0500

    Implement message function

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ActiveRecord::Base
   # Remember to create a migration!
   has_secure_password
 
-  has_many :messages
-
   has_many :sent_messages, foreign_key: "sender_id", class_name: "Message"
   has_many :received_messages, foreign_key: "receiver_id", class_name: "Message"
 
