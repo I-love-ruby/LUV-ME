d788b20fbd7cbff8bf67f11e6e7d0c3123718bda
Jonathan Cho <joncho26@gmail.com>
2015-01-17 21:40:53 -0500

    add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,5 +20,6 @@ class User < ActiveRecord::Base
 
   validates :name, presence:true
   validates :email, presence:true
+  validates :email, format: {with: /[1-9]*[a-z]*\d*@[a-zA-z]*.[a-z]{3}/}
 
 end
