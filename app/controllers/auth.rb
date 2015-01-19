bf00a843540a0809ce859b632d01eeff9d31add8
Ricky-Thomas <rickythomas.rt@gmail.com>
2015-01-18 19:54:54 -0500

    Adds errors to login/signup routes

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -5,16 +5,19 @@ post '/login' do
     session[:user_id] = @user.id
     erb :home
   end
+  set_error('Login Failed.')
   redirect "/"
 end
 
 post '/signup' do
   user = User.new(params[:user])
-  if user.save
+  if user.valid?
     session[:user_id] = user.id
     redirect "/user/#{user.id}/edit"
+  else
+    set_error('Signup failed.')
+    redirect "/"
   end
-  redirect "/"
 end
 
 get '/signout' do
